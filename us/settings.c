5746026cc5760ab7ce736e25699f3b6b37bf5bed
Takashi Toyoshima <toyoshim@gmail.com>
2023-05-02 15:45:11 +0900

    user data version check

--- us/settings.c
+++ us/settings.c
@@ -9,10 +9,11 @@
 static __code uint8_t* flash = (__code uint8_t*)0xf000;
 
 static struct settings settings;
+static uint8_t current_setting = 0;  // valid range is [0...5]
 
 bool settings_init() {
   if (flash[0] != 'I' || flash[1] != 'O' || flash[2] != 'N' ||
-      flash[3] != 'C') {
+      flash[3] != 'C' || flash[4] != 1) {
     return false;
   }
   return true;

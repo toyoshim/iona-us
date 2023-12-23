c0e68b30bccfac8cacfdaa15940c1acb9c1242f6
Takashi Toyoshima <toyoshim@gmail.com>
2023-12-23 18:13:36 +0900

    generate button map

--- docs/layout_map.js
+++ docs/layout_map.js
@@ -1,13 +1,13 @@
-function createTr(...args) {
+function createTr(header, args) {
   const tr = document.createElement('tr');
   for (const arg of args) {
-    const td = document.createElement('td');
+    const holder = document.createElement(header ? 'th' : 'td');
     if (typeof arg === 'string') {
-      td.appendChild(document.createTextNode(arg));
+      holder.appendChild(document.createTextNode(arg));
     } else {
-      td.appendChild(arg);
+      holder.appendChild(arg);
     }
-    tr.appendChild(td);
+    tr.appendChild(holder);
   }
   return tr;
 }
@@ -41,19 +41,37 @@ function createSequence(prefix) {
   return span;
 }
 
+function createPair(a, b) {
+  const span = document.createElement('span');
+  span.appendChild(document.createTextNode(a));
+  span.appendChild(document.createElement('br'));
+  span.appendChild(document.createTextNode(b));
+  return span;
+}
+
+function createCheckboxPair(i, o) {
+  const span = document.createElement('span');
+  span.appendChild(createCheckbox(i + '_p1' + o));
+  span.appendChild(document.createElement('br'));
+  span.appendChild(createCheckbox(i + '_p2' + o));
+  return span;
+}
+
 const analog_map = document.getElementById('analog_map');
-const analog_tbody = document.createElement('tbody');
-analog_tbody.appendChild(createTr(
+const analog_thead = document.createElement('thead');
+analog_thead.appendChild(createTr(true, [
   uiMessages.layoutAnalogInput,
   uiMessages.layoutAnalogOutputType,
   uiMessages.layoutAnalogOutputNumber,
-  uiMessages.layoutAnalogInvert,
-));
+  uiMessages.layoutAnalogInvert
+]));
+analog_map.appendChild(analog_thead);
+const analog_tbody = document.createElement('tbody');
 const players = ['1', '2'];
 const ids = ['1 (LX)', '2 (LY)', '3 (RX)', '4 (RY)', '5 (LT)', '6 (RT)'];
 for (let player = 0; player < players.length; ++player) {
   for (let id = 0; id < ids.length; ++id) {
-    analog_tbody.appendChild(createTr(
+    analog_tbody.appendChild(createTr(false, [
       'P' + players[player] + ' ' + uiMessages.layoutAnalogAnalog + ' ' + ids[id],
       createSelect('a' + (player + 1) + (id + 1) + 't', [
         uiMessages.layoutAnalogOutputTypeNone,
@@ -64,25 +82,81 @@ for (let player = 0; player < players.length; ++player) {
       ]),
       createSelect('a' + (player + 1) + (id + 1) + 'i', [0, 1, 2, 3, 4, 5, 6, 7]),
       createCheckbox('a' + (player + 1) + (id + 1) + 'r'),
-    ));
+    ]));
   }
 }
 analog_map.appendChild(analog_tbody);
 
 const rapid_fire_map = document.getElementById('rapid_fire_map');
-const rapid_fire_tbody = document.createElement('tbody');
-rapid_fire_tbody.appendChild(createTr(
+const rapid_fire_thead = document.createElement('thead');
+rapid_fire_thead.appendChild(createTr(true, [
   uiMessages.layoutRapidFireSet,
   uiMessages.layoutRapidFireSequence,
   uiMessages.layoutRapidFireCycle,
   uiMessages.layoutRapidFireInvert,
-));
+]));
+rapid_fire_map.appendChild(rapid_fire_thead);
+const rapid_fire_tbody = document.createElement('tbody');
 for (let set = 0; set < 7; ++set) {
-  rapid_fire_tbody.appendChild(createTr(
+  rapid_fire_tbody.appendChild(createTr(false, [
     uiMessages.layoutRapidFireSetPrefix + ' ' + (set + 1),
     createSequence('p' + (set + 1)),
     createSelect('rm' + (set + 1), [1, 2, 3, 4, 5, 6, 7, 8]),
     createCheckbox('inv' + (set + 1)),
-  ));
+  ]));
 }
 rapid_fire_map.appendChild(rapid_fire_tbody);
+
+const button_map = document.getElementById('button_map');
+const buttons = ['↑', '↓', '←', '→', '□', '✕', '○', '△', 'L1', 'R1', 'L2', 'R2', 'Share', 'Option', 'L3', 'R3'];
+const buttonIds = ['u', 'd', 'l', 'r', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c'];
+for (let player = 0; player < 2; ++player) {
+  const button_thead = document.createElement('thead');
+  const inout = createPair(
+    ' \\' + uiMessages.layoutButtonOutput,
+    uiMessages.layoutButtonInput);
+  button_thead.appendChild(createTr(true, [
+    inout,
+    '',
+    'Start', 'Coin', '↑', '↓', '←', '→', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+    uiMessages.layoutButtonRapidFire,
+  ]));
+  button_map.appendChild(button_thead);
+  const button_tbody = document.createElement('tbody');
+  for (let button = 0; button < 16; ++button) {
+    const prefix = 'p' + players[player] + buttonIds[button];
+    button_tbody.appendChild(createTr(false, [
+      'P' + players[player] + ' ' + buttons[button],
+      createPair('P1', 'P2'),
+      createCheckboxPair(prefix, 's'),
+      createCheckboxPair(prefix, 'c'),
+      createCheckboxPair(prefix, 'u'),
+      createCheckboxPair(prefix, 'd'),
+      createCheckboxPair(prefix, 'l'),
+      createCheckboxPair(prefix, 'r'),
+      createCheckboxPair(prefix, '1'),
+      createCheckboxPair(prefix, '2'),
+      createCheckboxPair(prefix, '3'),
+      createCheckboxPair(prefix, '4'),
+      createCheckboxPair(prefix, '5'),
+      createCheckboxPair(prefix, '6'),
+      createCheckboxPair(prefix, '7'),
+      createCheckboxPair(prefix, '8'),
+      createCheckboxPair(prefix, '9'),
+      createCheckboxPair(prefix, 'a'),
+      (button < 4) ? '' : createSelect(prefix + '_rp', [
+        uiMessages.layoutButtonRapidFireNone,
+        uiMessages.layoutButtonRapidFirePrefix + '1',
+        uiMessages.layoutButtonRapidFirePrefix + '2',
+        uiMessages.layoutButtonRapidFirePrefix + '3',
+        uiMessages.layoutButtonRapidFirePrefix + '4',
+        uiMessages.layoutButtonRapidFirePrefix + '5',
+        uiMessages.layoutButtonRapidFirePrefix + '6',
+        uiMessages.layoutButtonRapidFirePrefix + '7',
+        uiMessages.layoutButtonRapidFireGearUp,
+        uiMessages.layoutButtonRapidFireGearDown
+      ]),
+    ]));
+  }
+  button_map.appendChild(button_tbody);
+}
